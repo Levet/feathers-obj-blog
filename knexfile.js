a363aaa659dd74809139c70b247a5021479a7400
Jordan Levet <jlevet@coltbuilders.com>
2020-11-23 16:11:02 -0700

    Setting up .env for secrets

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,16 @@
 // Update with your config settings.
 
+require("dotenv").config()
+
+console.log(process.env.LOCAL_DB, process.env.LOCAL_DB_USER, process.env.LOCAL_DB_PASSWORD)
+
 module.exports = {
 
   development: {
     client: 'postgresql',
     connection: {
       database: process.env.LOCAL_DB,
-      user:     process.env.LOCAL_DB_USER,
+      user: process.env.LOCAL_DB_USER,
       password: process.env.LOCAL_DB_PASSWORD
     },
     pool: {
